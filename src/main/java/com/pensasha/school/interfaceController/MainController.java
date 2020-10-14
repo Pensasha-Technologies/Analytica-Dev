52fb99b07e0ea6af137f48a8c1c5bb9dbabac37f
Sobunge <samuelobunge@yahoo.com>
2020-10-14 14:14:40 +0300

    Updating adding marks

--- src/main/java/com/pensasha/school/interfaceController/MainController.java
+++ src/main/java/com/pensasha/school/interfaceController/MainController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.pensasha.school.exam.Mark;
 import com.pensasha.school.exam.MarkService;
