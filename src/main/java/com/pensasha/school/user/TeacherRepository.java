5cb984ef229f1b87c94ce1af0849275e3f94a802
Sobunge <samuelobunge@yahoo.com>
2020-10-17 21:53:44 +0300

    Updates

--- src/main/java/com/pensasha/school/user/TeacherRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pensasha.school.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TeacherRepository extends JpaRepository<Teacher, String> {
-
-}
