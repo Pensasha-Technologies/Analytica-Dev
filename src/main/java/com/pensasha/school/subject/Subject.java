166c6f3e6158e1fa5c67f41e7aaadab0328f4683
Sobunge <samuelobunge@yahoo.com>
2020-12-01 18:20:47 +0300

    Update

--- src/main/java/com/pensasha/school/subject/Subject.java
+++ src/main/java/com/pensasha/school/subject/Subject.java
@@ -8,7 +8,6 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.pensasha.school.form.Form;
